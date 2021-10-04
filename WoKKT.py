460744f70d0fc0e533c72ea82c210fd6569709bb
luisoibarra <luise98cu@gmail.com>
2021-10-04 10:22:49 -0400

    Errores mostrados con explicación, corregidos algunos typos

--- WoKKT.py
+++ WoKKT.py
@@ -61,11 +61,19 @@ cafe_b_ub = np.array([
     0,
     0,
 ])
+cafe_A_ub_meaning = [
+    "No tienes suficiente café colombiano para producir lo pactado",
+    "No tienes suficiente café cubano para producir lo pactado",
+    "No te alcanza el tiempo para producir lo pactado",
+    "No puedes hacer cantidades negativas de café super",
+    "No puedes hacer cantidades negativas de café deluxe",
+]
 
 cafe_A_eq = None
 
 cafe_b_eq = None
 
+cafe_A_eq_meaning = None
 
 ## UTILES DE OPTIMIZACION ##
 
@@ -85,7 +93,7 @@ def solve_linear_problem(maximizar, c, A_ub=None, b_ub=None, A_eq=None, b_eq=Non
         sol.fun = -sol.fun
     return sol
 
-def get_infringed_rules(sol, A_ub=None, b_ub=None, A_eq=None, b_eq=None):
+def get_infringed_rules(sol, A_ub=None, b_ub=None, A_eq=None, b_eq=None, A_ub_meaning=None, A_eq_meaning=None):
     infringed_rules = { "ub" : [], "eq" : []}
 
     # Determina las condiciones de desigualdad infringidas
@@ -96,7 +104,10 @@ def get_infringed_rules(sol, A_ub=None, b_ub=None, A_eq=None, b_eq=None):
     if A_eq is not None and A_eq.any() and b_eq is not None and b_eq.any():
         mul = A_eq @ sol == b_eq
         infringed_rules["eq"] = [i + 1 for i in range(len(mul)) if not mul[i]]
-
+    if A_ub_meaning:
+        infringed_rules["ub"] = [A_ub_meaning[i-1] for i in infringed_rules["ub"]]
+    if A_eq_meaning:
+        infringed_rules["eq"] = [A_eq_meaning[i-1] for i in infringed_rules["eq"]]
     return infringed_rules
 
 # Solucion al problema
@@ -127,7 +138,7 @@ def show_cafe_solution_report(cafe_super, cafe_deluxe):
     ganancia_max = cafe_solution.fun
     ganancia_hecha = cafe_c @ user_solution
     feasible_original_problem = cafe_solution.success
-    infringed_rules = get_infringed_rules(user_solution, cafe_A_ub, cafe_b_ub, cafe_A_eq, cafe_b_eq)
+    infringed_rules = get_infringed_rules(user_solution, cafe_A_ub, cafe_b_ub, cafe_A_eq, cafe_b_eq, cafe_A_ub_meaning, cafe_A_eq_meaning)
     feasible_user_solution = len(infringed_rules["eq"]) + len(infringed_rules["ub"]) == 0
     feasible_answer = True
 
@@ -136,29 +147,22 @@ def show_cafe_solution_report(cafe_super, cafe_deluxe):
         print("Solución dada de café super:" ,cafe_super)
         print("Solución dada de café deluxe:",cafe_deluxe)
     else:
-        print("Tu solución no cumple con las restricciones:")
-        if len(infringed_rules["ub"]) > 0:
-            print("De desigualdad en las condiciones: " + str(infringed_rules["ub"]))
-
-        if len(infringed_rules["eq"]) > 0:
-            print("De igualdad en las condiciones: " + str(infringed_rules["eq"]))
-
+        errores = infringed_rules["ub"] + infringed_rules["eq"]
+        if len(errores) > 0:
+            print("La solución no es válida debido a:\n" + "\t\n".join([str(x) for x in errores]))
         print()
         feasible_answer = False
     
     if feasible_answer:
         if ganancia_max>ganancia_hecha*4:
-            print('Tu solucion es bastante mala con respecto a la gancia máxima posible,es menos que la cuarta parte.La ganancia máxima era de',ganancia_max)
+            print('Tu solución es bastante mala con respecto a la ganancia máxima posible, es menos que la cuarta parte.La ganancia máxima era de',ganancia_max)
         elif ganancia_max>ganancia_hecha*2:
-            print('Tu solucion es mala con respecto a la gancia máxima posible,es menos de la mitad.La ganancia máxima era de',ganancia_max)
+            print('Tu solución es mala con respecto a la gancia máxima posible,es menos de la mitad.La ganancia máxima era de',ganancia_max)
        
         elif ganancia_max<ganancia_hecha*1.5:
-            print('Tu solucion es bastante buena la ganancia máxima era de',ganancia_max)
+            print('Tu solución es bastante buena, la ganancia máxima era de',ganancia_max)
         elif ganancia_max<ganancia_hecha+5:
-            print('Tu solucion es optima')
-
-
-
+            print('Tu solución es óptima')
     else:
         if feasible_original_problem:
 
@@ -190,7 +194,7 @@ def run():
     print(f"{cafe_super} Kg de café super y {cafe_deluxe} Kg de café deluxe")
     
     print()
-    show_spinner()
+    # show_spinner()
     print()
     
     show_cafe_solution_report(cafe_super, cafe_deluxe)

1692fea59ad11c0b134a3c25c52eb136df57f216
Luiso <luise98cu@gamil.com>
2022-02-06 11:53:31 -0500

    Completar TODOs para la simulación del juego

--- CastleDefense/castle_defense_discrete/castle_simulation.py
+++ CastleDefense/castle_defense_discrete/castle_simulation.py
@@ -11,8 +11,38 @@ class ModeloSimulacion(Modelo):
         """
         Recrea el juego en interacción con el usuario
         """
-        raise NotImplementedError
-
+        dia_actual = 0
+        total_de_dias = self.juego.estrategia_enemiga
+        
+        recursos_actuales = {x.nombre:x.cantidad for x in self.juego.castillo.recursos}
+        
+        while dia_actual < len(total_de_dias):
+            # TODO
+            # Imprimir el estado del dia actal
+            # - Poder de oleada
+            # - Recuros
+            # - Uso de los habitantes hasta ahora
+            # - etc
+            poder_enemigo_ronda_actual = self.juego.estrategia_enemiga[dia_actual]
+            print("Poder del enemigo en la oleada:", poder_enemigo_ronda_actual)
+            for rec_name in recursos_actuales:
+                print(rec_name, "->", recursos_actuales[rec_name])
+            
+            # TODO
+            # Recibir la entrada de las variables por parte del usuario y verificar
+            # que cumplan los requerimientos del problema
+            # - artesanos para contruir armas
+            # - armas construidas (tiene que estar previamente mandadas a constuir)
+            # - guerreros asignados a las armas (tienen que estar constuidas)
+            # - las armas con guerreros asignados son las que generan ataque
+            # - no exceder el numero máximo de guerreros y artesanos en las asignaciones por día
+            # - Actualizar el estado actual de los recursos etc
+            
+            # TODO
+            # Verificar si cumple que el ataque generado por las armas puede repeler la oleada enemiga
+            
+            
+            
 class JuegoSimulacion(Juego):
     
     def generar_modelo(self) -> Modelo:
